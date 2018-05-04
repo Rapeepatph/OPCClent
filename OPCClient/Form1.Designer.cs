03b3734ba351e8dacefb94912f15febda4b51efd
rapeepatph <rapeepat79@gmail.com>
2018-05-04 10:53:03 +0700

    Add project files.

---
+++ OPCClient/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace OPCClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowserOPCServer = new System.Windows.Forms.Button();
+            this.lstOPCServer = new System.Windows.Forms.TreeView();
+            this.txtNodeAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusConnection = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ExceptionStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnBrowseOPCItems = new System.Windows.Forms.Button();
+            this.treeOPC = new System.Windows.Forms.TreeView();
+            this.lstOPCItems = new System.Windows.Forms.ListView();
+            this.dataOPC = new System.Windows.Forms.DataGridView();
+            this.ColumnItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAddToMonitoring = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOPC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowserOPCServer
+            // 
+            this.btnBrowserOPCServer.Location = new System.Drawing.Point(30, 41);
+            this.btnBrowserOPCServer.Name = "btnBrowserOPCServer";
+            this.btnBrowserOPCServer.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowserOPCServer.TabIndex = 0;
+            this.btnBrowserOPCServer.Text = "BrowserOPCServer";
+            this.btnBrowserOPCServer.UseVisualStyleBackColor = true;
+            this.btnBrowserOPCServer.Click += new System.EventHandler(this.btnBrowserOPCServer_Click);
+            // 
+            // lstOPCServer
+            // 
+            this.lstOPCServer.Location = new System.Drawing.Point(30, 80);
+            this.lstOPCServer.Name = "lstOPCServer";
+            this.lstOPCServer.Size = new System.Drawing.Size(284, 141);
+            this.lstOPCServer.TabIndex = 1;
+            // 
+            // txtNodeAddress
+            // 
+            this.txtNodeAddress.Location = new System.Drawing.Point(128, 43);
+            this.txtNodeAddress.Name = "txtNodeAddress";
+            this.txtNodeAddress.Size = new System.Drawing.Size(186, 20);
+            this.txtNodeAddress.TabIndex = 2;
+            this.txtNodeAddress.Text = "localhost";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(30, 227);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.StatusConnection,
+            this.toolStripStatusLabel2,
+            this.ExceptionStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 605);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(943, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(78, 17);
+            this.toolStripStatusLabel1.Text = "Connection : ";
+            // 
+            // StatusConnection
+            // 
+            this.StatusConnection.ForeColor = System.Drawing.Color.Blue;
+            this.StatusConnection.Name = "StatusConnection";
+            this.StatusConnection.Size = new System.Drawing.Size(84, 17);
+            this.StatusConnection.Text = "not connected";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.Color.Gray;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(67, 17);
+            this.toolStripStatusLabel2.Text = "Exception : ";
+            // 
+            // ExceptionStatus
+            // 
+            this.ExceptionStatus.Name = "ExceptionStatus";
+            this.ExceptionStatus.Size = new System.Drawing.Size(15, 17);
+            this.ExceptionStatus.Text = " -";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(128, 227);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnBrowseOPCItems
+            // 
+            this.btnBrowseOPCItems.Location = new System.Drawing.Point(226, 227);
+            this.btnBrowseOPCItems.Name = "btnBrowseOPCItems";
+            this.btnBrowseOPCItems.Size = new System.Drawing.Size(88, 23);
+            this.btnBrowseOPCItems.TabIndex = 6;
+            this.btnBrowseOPCItems.Text = "Browse Items";
+            this.btnBrowseOPCItems.UseVisualStyleBackColor = true;
+            this.btnBrowseOPCItems.Click += new System.EventHandler(this.btnBrowseOPCItems_Click);
+            // 
+            // treeOPC
+            // 
+            this.treeOPC.Location = new System.Drawing.Point(30, 265);
+            this.treeOPC.Name = "treeOPC";
+            this.treeOPC.Size = new System.Drawing.Size(137, 304);
+            this.treeOPC.TabIndex = 7;
+            this.treeOPC.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeOPC_NodeMouseClick);
+            // 
+            // lstOPCItems
+            // 
+            this.lstOPCItems.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lstOPCItems.Location = new System.Drawing.Point(190, 265);
+            this.lstOPCItems.Name = "lstOPCItems";
+            this.lstOPCItems.ShowItemToolTips = true;
+            this.lstOPCItems.Size = new System.Drawing.Size(124, 304);
+            this.lstOPCItems.TabIndex = 16;
+            this.lstOPCItems.UseCompatibleStateImageBehavior = false;
+            // 
+            // dataOPC
+            // 
+            this.dataOPC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataOPC.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnItem,
+            this.ColumnValue});
+            this.dataOPC.Enabled = false;
+            this.dataOPC.Location = new System.Drawing.Point(402, 80);
+            this.dataOPC.Name = "dataOPC";
+            this.dataOPC.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataOPC.Size = new System.Drawing.Size(343, 344);
+            this.dataOPC.TabIndex = 17;
+            // 
+            // ColumnItem
+            // 
+            this.ColumnItem.HeaderText = "Item";
+            this.ColumnItem.Name = "ColumnItem";
+            this.ColumnItem.Width = 200;
+            // 
+            // ColumnValue
+            // 
+            this.ColumnValue.HeaderText = "Value";
+            this.ColumnValue.Name = "ColumnValue";
+            // 
+            // btnAddToMonitoring
+            // 
+            this.btnAddToMonitoring.Location = new System.Drawing.Point(190, 576);
+            this.btnAddToMonitoring.Name = "btnAddToMonitoring";
+            this.btnAddToMonitoring.Size = new System.Drawing.Size(124, 23);
+            this.btnAddToMonitoring.TabIndex = 18;
+            this.btnAddToMonitoring.Text = "Add to Monitoring";
+            this.btnAddToMonitoring.UseVisualStyleBackColor = true;
+            this.btnAddToMonitoring.Click += new System.EventHandler(this.btnAddToMonitoring_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(402, 443);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 19;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 627);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnAddToMonitoring);
+            this.Controls.Add(this.dataOPC);
+            this.Controls.Add(this.lstOPCItems);
+            this.Controls.Add(this.treeOPC);
+            this.Controls.Add(this.btnBrowseOPCItems);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtNodeAddress);
+            this.Controls.Add(this.lstOPCServer);
+            this.Controls.Add(this.btnBrowserOPCServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOPC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowserOPCServer;
+        private System.Windows.Forms.TreeView lstOPCServer;
+        private System.Windows.Forms.TextBox txtNodeAddress;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusConnection;
+        internal System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel ExceptionStatus;
+        private System.Windows.Forms.Button btnBrowseOPCItems;
+        internal System.Windows.Forms.TreeView treeOPC;
+        internal System.Windows.Forms.ListView lstOPCItems;
+        internal System.Windows.Forms.DataGridView dataOPC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnValue;
+        internal System.Windows.Forms.Button btnAddToMonitoring;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ OPCClient/Form1.cs
@@ -0,0 +1,378 @@
+﻿
+using OPCAutomation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OPCClient
+{
+    public struct strOPCServer
+    {
+        public OPCServer MyServer;
+        public OPCGroup MyGroup;
+        public OPCItem MyItem;
+        public Array ServerHandles;
+        public Array Errors;
+        public Array Values;
+        public int MonCount;
+    }
+    public struct strcMonItem
+    {
+        public int _Server;
+        public string _Name;
+        public object _Value;
+        public int _Index;
+        public object _ServerHandles;
+    }
+
+    public partial class Form1 : Form
+    {
+        OPCServer MyOPCServers = new OPCServer();
+        strOPCServer[] MyOPCServer = new strOPCServer[11];
+        OPCGroup[] MyOPCGroup = new OPCGroup[11];
+
+
+
+        string treePath;
+        OPCBrowser MyOPCBrowser;
+
+        strcMonItem[] MonItem;
+        int MonItem_Count = 0;
+        int iOPC;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        private void btnBrowserOPCServer_Click(object sender, EventArgs e)
+        {
+            object ListServers = MyOPCServers.GetOPCServers(txtNodeAddress.Text);
+            lstOPCServer.Nodes.Clear();
+            var servers = ((Array)(ListServers));
+            foreach(string server in servers)
+            {
+                lstOPCServer.Nodes.Add(server);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+               MyOPCServer[iOPC].MyServer.Connect(lstOPCServer.SelectedNode.Text, txtNodeAddress.Text);
+               if (MyOPCServer[iOPC].MyServer.ServerState == 1)
+               {
+                   StatusConnection.Text = "Connected";
+                   lstOPCServer.SelectedNode.ForeColor = Color.Green;
+                   btnConnect.Enabled = false;
+                    btnDisconnect.Enabled = true;
+                    MonItem_Count = dataOPC.RowCount - 1;
+
+                    if (MyOPCGroup[iOPC] == null)
+                    {
+                        MyOPCGroup[iOPC] = MyOPCServer[iOPC].MyServer.OPCGroups.Add("MyOPC_" + iOPC);
+                    }
+                    MyOPCGroup[iOPC].IsActive = false;
+                    MyOPCGroup[iOPC].IsSubscribed = true;
+                    MyOPCGroup[iOPC].DataChange += new DIOPCGroupEvent_DataChangeEventHandler(MyOPCGroup_DataChange);
+                    //MyOPCGroup[iOPC].AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(MyOPCGroup_AsyncWriteComplete);
+                    MyOPCGroup[iOPC].AsyncReadComplete += new DIOPCGroupEvent_AsyncReadCompleteEventHandler(MyOPCGroup_AsyncReadComplete);
+               }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please Browse Server Name", "Alert", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                ExceptionStatus.Text = ex.Message;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                MyOPCServer[i].MyServer = new OPCServer();
+                MyOPCServer[i].ServerHandles = new int[1000];
+                MyOPCServer[i].Errors = new int[1000];
+                MyOPCServer[i].Values = new object[1000];
+            }
+        }
+        //private void lstOPCServer_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        //{
+        //    lstOPCServer.SelectedNode = lstOPCServer.GetNodeAt(e.X, e.Y);
+
+        //}
+        //private void lstOPCServer_AfterSelect(object sender, TreeViewEventArgs e)
+        //{
+        //    for (int i = 0; i <= lstOPCServer.Nodes.Count - 1; i++)
+        //    {
+        //        lstOPCServer.Nodes[i].BackColor = Color.White;
+        //    }
+        //    lstOPCServer.SelectedNode.BackColor = Color.Cyan;
+        //    iOPC = lstOPCServer.SelectedNode.Index;
+        //}
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool okDiskonek = true;
+                for (int i = 0; i <= 10; i++)
+                {
+                    if (MyOPCServer[i].MyServer.ServerState == 1)
+                    {
+                        MyOPCServer[i].MyServer.Disconnect();
+                        if (MyOPCServer[i].MyServer.ServerState == 1)
+                        {
+                            okDiskonek = false;
+                        }
+                    }
+                }
+                if (okDiskonek)
+                {
+                    StatusConnection.Text = "Disconnected";
+                    lstOPCServer.SelectedNode.ForeColor = Color.Black;
+                    btnConnect.Enabled = true;
+                    btnDisconnect.Enabled = false;
+                    treeOPC.Nodes.Clear();
+                    lstOPCItems.Items.Clear();
+                }
+                else
+                {
+                    StatusConnection.Text = "ERROR Disconnect!";
+                }
+            }
+            catch(Exception ex)
+            {
+                StatusConnection.Text = "ERROR Disconnect!  ";
+                ExceptionStatus.Text = ex.Message;
+            }
+            
+        }
+
+        private void btnBrowseOPCItems_Click(object sender, EventArgs e)
+        {
+            lstOPCItems.Items.Clear();
+            treeOPC.Nodes.Clear();
+            treePath = "";
+            if (lstOPCServer.SelectedNode.ForeColor != Color.Green)
+            {
+                return;
+            }
+
+            MyOPCBrowser = MyOPCServer[iOPC].MyServer.CreateBrowser();
+            MyOPCBrowser.MoveToRoot();
+            Show_Leafs(MyOPCBrowser);
+
+            MyOPCBrowser.MoveToRoot();
+            Show_Branches(MyOPCBrowser, treeOPC.SelectedNode);
+        }
+
+        public void Show_Leafs(OPCBrowser MyOPCBrowser)
+        {
+            MyOPCBrowser.ShowLeafs();
+            int a;
+            dynamic iA = null;
+            iA = MyOPCBrowser.Count;
+            string name1 = "";
+            lstOPCItems.Items.Clear();
+            for (a = 1; a <= iA; a++)
+            {
+                name1 = MyOPCBrowser.Item(a).ToString();
+                string name2 = name1.ToLower();
+                lstOPCItems.Items.Add(name1);
+            }
+        }
+
+        public void Show_Branches(OPCBrowser MyOPCBrowser, TreeNode aNode)
+        {
+            MyOPCBrowser.ShowBranches();
+            if ((aNode != null))
+            {
+                aNode.Nodes.Clear();
+            }
+            dynamic iA = null;
+            dynamic iB = null;
+            iA = MyOPCBrowser.Count;
+            for (int a = 1; a <= iA; a++)
+            {
+                try
+                {
+                    string sName = MyOPCBrowser.Item(a);
+                    if (aNode == null)
+                    {
+                        treeOPC.Nodes.Add(sName);
+                    }
+                    else
+                    {
+                        aNode.Nodes.Add(sName);
+                    }
+                    MyOPCBrowser.MoveDown(sName);
+                    MyOPCBrowser.ShowBranches();
+                    iB = MyOPCBrowser.Count;
+                    if (iB > 0)
+                    {
+                        if (aNode == null)
+                        {
+                            treeOPC.Nodes[a - 1].Nodes.Add("-");
+                        }
+                        else
+                        {
+                            aNode.Nodes[a - 1].Nodes.Add("-");
+                        }
+                    }
+                    MyOPCBrowser.MoveUp();
+                }
+                catch (Exception ex)
+                {
+                    ExceptionStatus.Text = ex.Message;
+                }
+                MyOPCBrowser.ShowBranches();
+            }
+        }
+
+        private void treeOPC_NodeMouseClick(object sender, System.Windows.Forms.TreeNodeMouseClickEventArgs e)
+        {
+            treeOPC.SelectedNode = treeOPC.GetNodeAt(e.X, e.Y);
+            try
+            {
+                string nPath = null;
+                nPath = e.Node.FullPath;
+
+                if (nPath == treePath)
+                {
+                    return;
+                }
+                else
+                {
+                    treePath = nPath;
+                }
+
+                string[] sPath;
+                string[] sBranch = new string[11];
+                int branchCount = 0;
+
+                sPath = nPath.Split('\\');
+                branchCount = sPath.Count();
+                for (int i = 0; i <= branchCount - 1; i++)
+                {
+                    sBranch[i] = sPath[i];
+                }
+
+
+                
+                MyOPCBrowser = MyOPCServer[iOPC].MyServer.CreateBrowser();
+
+                MyOPCBrowser.MoveToRoot();
+                for (int i = 0; i <= branchCount - 1; i++)
+                {
+                    MyOPCBrowser.ShowBranches();
+                    MyOPCBrowser.MoveDown(sBranch[i]);
+                }
+                Show_Leafs(MyOPCBrowser);
+                Show_Branches(MyOPCBrowser, treeOPC.SelectedNode);
+
+            }
+            catch //(Exception ex)
+            {
+                //
+            }
+        }
+
+        private void btnAddToMonitoring_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataOPC.Enabled = true;
+                dynamic indeks = lstOPCItems.SelectedIndices[0];
+                dynamic icount = lstOPCItems.SelectedItems.Count;
+                for (int i = indeks; i <= (indeks + icount - 1); i++)
+                {
+                    string itemName = null;
+                    itemName = MyOPCBrowser.GetItemID(lstOPCItems.Items[i].Text);
+
+                    dataOPC.Rows.Add();
+                    dataOPC[0, MonItem_Count].Value = itemName;
+
+                    MonItem_Count += 1;
+
+                    Array.Resize(ref MonItem, MonItem_Count + 1);
+                    MonItem[MonItem_Count]._Server = iOPC;
+                    MonItem[MonItem_Count]._Name = itemName;
+                    MonItem[MonItem_Count]._Index = dataOPC.RowCount - 1;
+
+                    MyOPCServer[iOPC].MyItem = MyOPCGroup[iOPC].OPCItems.AddItem(itemName, MonItem_Count);
+                    MyOPCServer[iOPC].ServerHandles.SetValue(MyOPCServer[iOPC].MyItem.ServerHandle, MonItem_Count);
+
+                }
+                lstOPCItems.SelectedIndices.Clear();
+            }
+            catch(Exception ex)
+            {
+                ExceptionStatus.Text = ex.Message;
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MonItem_Count == 0)
+                {
+                    SubscribeItems();
+                }
+
+                int numItems = MonItem_Count;
+                int transID;
+                MyOPCGroup[iOPC].AsyncRead(numItems, MyOPCServer[iOPC].ServerHandles, out MyOPCServer[iOPC].Errors, DateTime.Now.Second, out transID);
+
+                ExceptionStatus.Text = "OK Read";
+            }
+            catch (Exception ex)
+            {
+                ExceptionStatus.Text = "ERROR Read - " + ex.Message;
+            }
+        }
+        public void SubscribeItems()
+        {
+            for (int i = 1; i <= dataOPC.RowCount - 1; i++)
+            {
+                string itemName = MonItem[i]._Name;
+                //int iServer = MonItem[i]._Server;
+                MyOPCServer[iOPC].MyItem = MyOPCServer[iOPC].MyGroup.OPCItems.AddItem(itemName, i);
+                MyOPCServer[iOPC].ServerHandles.SetValue(MyOPCServer[iOPC].MyItem.ServerHandle, i);
+                MonItem_Count += 1;
+            }
+        }
+        private void MyOPCGroup_DataChange(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps)
+        {
+            for (int i = 1; i <= NumItems; i++)
+            {
+                int iHandles = Convert.ToInt32(ClientHandles.GetValue(i));
+                dataOPC[1, iHandles - 1].Value = ItemValues.GetValue(i);
+                dataOPC[1, iHandles - 1].Style.BackColor = Color.White;
+            }
+        }
+        private void MyOPCGroup_AsyncReadComplete(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps, ref Array Errors)
+        {
+            for (int i = 1; i <= NumItems; i++)
+            {
+                int iHandles = Convert.ToInt32(ClientHandles.GetValue(i));
+                dataOPC[1, iHandles - 1].Value = ItemValues.GetValue(i);
+                dataOPC[1, iHandles - 1].Style.BackColor = Color.White;
+            }
+            ExceptionStatus.Text = "OK AsyncRead";
+        }
+    }
+    
+}
